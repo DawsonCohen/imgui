0aaf66103711d2ea08cec9da8844cd1e462c31e2
Dawson Cohen <dawson@the-cohens.com>
2023-12-02 20:05:03 -0500

    changed build path

--- premake5.lua
+++ premake5.lua
@@ -2,8 +2,8 @@ project "ImGui"
 	kind "StaticLib"
 	language "C++"
 
-	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
-	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+	targetdir ("build-bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("build-obj/" .. outputdir .. "/%{prj.name}")
 
 	includedirs
 	{
